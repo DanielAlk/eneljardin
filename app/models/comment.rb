1dc93327e98c3df65a211f4735fa03c25d1113be
DanielAlk <daniodfad@yahoo.com.ar>
2016-10-16 17:07:43 -0300

    add comment role enum

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,6 @@ class Comment < ActiveRecord::Base
   belongs_to :commentable, polymorphic: true
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
+
+  enum role: [:comment]
 end
