2e5cc2952aa34e2b48ad1d10b2c393aa4ed0bce7
DanielAlk <daniodfad@yahoo.com.ar>
2016-10-24 09:48:18 -0300

    newsletter form

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,6 @@
 class Contact < ActiveRecord::Base
-	validates_presence_of [:name, :email, :message]
+	validates_presence_of :email
+	validates_presence_of [:name, :message], unless: :newsletter?
 
 	enum kind: [:contact, :newsletter]
 end
